021774017476f2e6edcdeb70aff677254d611201
farrellw <farrell.will.g@gmail.com>
2018-05-16 16:11:54 -0500

    Add html template with input. Add search method to trigger dispatch of events and searching of members.

--- src/app/search-form/search-form.component.ts
+++ src/app/search-form/search-form.component.ts
@@ -1,19 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Store, select } from '@ngrx/store';
+import { State } from "../app.reducer";
+import { Observable } from 'rxjs'
+import { AppActionType } from '../app.actions';
+import { MemberService } from '../services/member.service';
 
+
+//TODO what does the async option mean??
 @Component({
   selector: 'search-form',
   template: `
-    <p>
-      search-form works!
-    </p>
+    <div>
+      <input placeholder="Search" (keyup.enter)="search($event)">
+      <p>{{ (searchingState | async)['isSearching'] ? "Searching" : "Not Searching" }}</p>
+    </div>
   `,
   styles: []
 })
 export class SearchFormComponent implements OnInit {
+  private searchingState: Observable<State>;
 
-  constructor() { }
+  constructor(private store: Store<State>, private memberService: MemberService) { 
+    this.searchingState = store.pipe(select('appStore'));
+  }
 
   ngOnInit() {
+  }
+
+  search($event){
+    this.store.dispatch({ type: AppActionType.Search});
+    this.memberService.searchMembers($event.target.value).subscribe(result => {
+      this.store.dispatch({ type: AppActionType.SearchCompleted, payload: result})
+    })
   }
 
 }
