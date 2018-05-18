b6917adcd7f7f2b651d624c2036e5815a127d607
Abadi Kurniawan <abadi.kurniawan@gmail.com>
2018-05-18 11:00:05 -0500

    Fix existing test

--- src/app/search-result/search-result.component.spec.ts
+++ src/app/search-result/search-result.component.spec.ts
@@ -1,20 +1,24 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { SearchResultComponent } from './search-result.component';
-import { MemberTableComponent } from '../member-table/member-table.component';
-import { StoreModule } from '@ngrx/store';
-import { reducer } from '../app.reducer';
+import { SearchResultComponent } from "./search-result.component";
+import { MemberTableComponent } from "../member-table/member-table.component";
+import { LoadingIndicatorComponent } from "../loading-indicator/loading-indicator.component";
+import { StoreModule } from "@ngrx/store";
+import { reducer } from "../app.reducer";
 
-describe('SearchResultComponent', () => {
+describe("SearchResultComponent", () => {
   let component: SearchResultComponent;
   let fixture: ComponentFixture<SearchResultComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [SearchResultComponent, MemberTableComponent],
+      declarations: [
+        SearchResultComponent,
+        MemberTableComponent,
+        LoadingIndicatorComponent
+      ],
       imports: [StoreModule.forRoot({ appStore: reducer })]
-    })
-      .compileComponents();
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -23,7 +27,7 @@ describe('SearchResultComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
